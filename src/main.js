5edcde5414d76e14ee591b45e0ae26970440b897
Juan Salazar <ferthelet@gmail.com>
2023-05-15 22:26:25 -0500

    fixes fetching/displaying multiple cats

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@
 function displayCat(catId) {
     var img = document.createElement("img");
 
-    img.id = imgId;
+    img.id = catId;
     img.src = `https://cataas.com/cat/${catId}`;
     img.classList.add("imageFeatures");
     img.alt = `cute cat ${catId}`;
     document.body.appendChild(img);
 }
 
-function fetchCats() {
-    const requestURL = "https://cataas.com/api/cats?limit=5";
+function fetchCats(quantity = 1) {
+    const requestURL = `https://cataas.com/api/cats?limit=${quantity}`;
+    debugger;
     const request = new Request(requestURL);
 
     const response = fetch(request);
@@ -19,24 +20,14 @@ function fetchCats() {
         .then((response) => response.json())
         .then((data) => {
             data.forEach((cat) => {
-                displayCat(cat.id);
-                console.log(cat.id);
+                displayCat(cat._id);
+                console.log(cat._id);
             });
         })
         .catch((error) => console.log(error));
 }
 
 
-//     fetch("https://cataas.com/cat?json=true&height=200")
-//         .then((response) => response.json())
-//         .then((data) => {
-//             const image = document.getElementById('catImg');
-//             const imageUrl = data.url;
-//             displayImage(imageUrl.slice(-16));
-//         })
-//         .catch((error) => console.log(error));
-// }
-
 
 const myButton = document.getElementById('reload');
-myButton.addEventListener('click', fetchCats);
+myButton.addEventListener('click', fetchCats(5));
