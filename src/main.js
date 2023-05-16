d90517c635dbdcae301814a948d22664b59d3b74
Juan Salazar <ferthelet@gmail.com>
2023-05-15 22:01:40 -0500

    replacing single cat request/display for multiple

--- src/main.js
+++ src/main.js
@@ -1,25 +1,41 @@
 // js
 
-function displayImage(imgId) {
+function displayCat(catId) {
     var img = document.createElement("img");
 
     img.id = imgId;
-    img.src = `https://cataas.com/cat/${imgId}`;
+    img.src = `https://cataas.com/cat/${catId}`;
     img.classList.add("imageFeatures");
-    img.alt = `cute cat ${imgId}`;
+    img.alt = `cute cat ${catId}`;
     document.body.appendChild(img);
 }
 
 function fetchCats() {
-    fetch("https://cataas.com/cat?json=true")
+    const requestURL = "https://cataas.com/api/cats?limit=5";
+    const request = new Request(requestURL);
+
+    const response = fetch(request);
+    response
         .then((response) => response.json())
         .then((data) => {
-            const image = document.getElementById('catImg');
-            const imageUrl = data.url;
-            displayImage(imageUrl.slice(-16));
+            data.forEach((cat) => {
+                displayCat(cat.id);
+                console.log(cat.id);
+            });
         })
         .catch((error) => console.log(error));
 }
+
+
+//     fetch("https://cataas.com/cat?json=true&height=200")
+//         .then((response) => response.json())
+//         .then((data) => {
+//             const image = document.getElementById('catImg');
+//             const imageUrl = data.url;
+//             displayImage(imageUrl.slice(-16));
+//         })
+//         .catch((error) => console.log(error));
+// }
 
 
 const myButton = document.getElementById('reload');
