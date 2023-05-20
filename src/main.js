8438e93187d3c98ebe800f7e770fa8da62c73a85
Juan Salazar <ferthelet@gmail.com>
2023-05-20 17:03:28 -0500

    replaces fetch with async await trying to better/ease the async requests control

--- src/main.js
+++ src/main.js
@@ -7,13 +7,14 @@ function displayDeadCat(catId) {
     const img = document.createElement("img");
 }
 
-// remove cat from list
-function removeListCat(catId) {
-    const catListDiv = document.getElementById("catList");
-    const catImg = document.getElementById(catId);
+// remove cat from list :: TODO :: check if this is needed
+// function removeListCat(catId) {
+//     const catListDiv = document.getElementById("catList");
+//     const catImg = document.getElementById(catId);
+//     debugger;
 
-    catListDiv.removeChild(catImg);
-}
+//     catListDiv.removeChild(catImg);
+// }
 
 // changes list cat to dead cat
 function changeListCat(catId) {
@@ -33,21 +34,31 @@ function changeListCat(catId) {
 function displayCat(catId) {
     const catImgDiv = document.getElementById("catImg");
     const catImg = document.createElement("img");
-   
-    catImg.src = `https://cataas.com/cat/${catId.substring(0, 16)}`; // 15 is the length of catId
-    catImg.alt = `${catId}`;
+
+    catImg.src = `https://cataas.com/cat/${catId.substring(0, 16)}`; // removes "-", 16 is the length of catId
+    catImg.alt = "shown cat";
     catImg.classList.add("shownCat");
     catImgDiv.insertAdjacentElement("afterbegin", catImg);
 
     changeListCat(catId);
 }
 
-// randomize catList
+// replace catId in catList with random catId array
+function replaceCatId() {
+    const catListClass = document.getElementsByClassName("listCat");
+
+    for (let i = 0; i < catListClass.length; i++) {
+        catListClass[i].id = catList[i];
+        debugger;
+    }
+}
+
+// randomize jokers in catList
 function randomizeCatList() {
     let currentIndex = catList.length;
     let temporaryValue;
     let randomIndex;
-    
+
     while (0 !== currentIndex) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -68,21 +79,19 @@ function displayJoker(catId) {
     img.addEventListener('click',
         () => displayCat(catId), { once: true });
     img.classList.add("listCat");
-    img.alt = `${catId}`;
+    img.alt = "joker cat";
     catListDiv.appendChild(img);
+    // randomizeCatList();
+    // replaceCatId();
 }
 
 // fetch one cat
 async function fetchOneCat() {
-    return (await fetch("https://cataas.com/cat?json=true"))
-        .json()
-        .then((data) => {
-            catList.push(data._id);
-            displayJoker(data._id);
-            catList.push(data._id + "-");
-            displayJoker(data._id + "-");
-            return data._id;
-        });
+    const data = await fetch("https://cataas.com/cat?json=true");
+    const cat = await data.json();
+    catList.push(cat._id);
+    displayJoker(cat._id);
+    return cat._id;
 }
 
 // display jokers list
@@ -92,12 +101,10 @@ function displayJokersList() {
 }
 
 // fetch cats
-async function fetchCats(qtty = 1) {
-    let counter = 0;
+function fetchCats(qtty = 1) {
 
-    while (counter < qtty) {
-        fetchOneCat();
-        counter++;
+    for (let i = 0; i < qtty; i++) {
+        const cat = fetchOneCat().then(catId => console.log(catId));
     }
 }
 
@@ -125,14 +132,14 @@ function handleFormSubmit(event) {
     catList = [];
 
 
-    if(easy.checked){
-       fetchCats(6);
+    if (easy.checked) {
+        fetchCats(6);
     }
-    if(medium.checked){
-       fetchCats(8);
+    if (medium.checked) {
+        fetchCats(8);
     }
-    if(hard.checked){
-       fetchCats(12);
+    if (hard.checked) {
+        fetchCats(12);
     }
 }
 
